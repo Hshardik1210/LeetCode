93081d7d49a9e8866a05cade243fe73fa5c14141
Hardik Sharma <hardik.sharma@whitehatjr.com>
2022-05-10 00:49:41 +0530

    clean

--- 139-word-break/139-word-break-set.cpp
+++ 139-word-break/139-word-break-set.cpp
@@ -1,20 +1,18 @@
 class Solution {
-    unordered_set<string>S;
+    unordered_set<string>dict;
 public:
     bool wordBreakUtil(string s) {
         if(s.length()==0)
             return true;
         for(int i=1;i<=s.length();i++) {
-            string temp1 = s.substr(0,i);
-            string temp2 = s.substr(i, s.length()-i);
-            if(S.find(s.substr(0,i))!=S.end() && wordBreakUtil(s.substr(i, s.length()-i)))
+            if(dict.find(s.substr(0,i))!=dict.end() && wordBreakUtil(s.substr(i, s.length()-i)))
                 return true;
         }
         return false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         for(int i=0;i<wordDict.size();i++) {
-            S.insert(wordDict[i]);
+            dict.insert(wordDict[i]);
         }
         return wordBreakUtil(s);
     }
