6775671a58663c3c133f3569cb8574e29a56669f
Hshardik1210 <104761960+Hshardik1210@users.noreply.github.com>
2022-05-13 01:50:49 +0530

    Time: 1863 ms (5.02%), Space: 641.4 MB (6.81%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,67 +1,82 @@
-struct Trie {
+struct Trie
+{
     bool isWord;
-    map<char,Trie*>children;
-    
-    Trie() {
-        isWord=false;
+    map<char, Trie*> children;
+
+    Trie()
+    {
+        isWord = false;
     }
 };
 
-void insert(string s, Trie* & dict) {
-    if(s.length()==0)
+void insert(string s, Trie* &dict)
+{
+    if (s.length() == 0)
         return;
-    Trie * node = dict;
-    for(int i=0;i<s.length();i++){
-        if(!node->children[s[i]])
-            node->children[s[i]]= new Trie();
+    Trie *node = dict;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (!node->children[s[i]])
+            node->children[s[i]] = new Trie();
         node = node->children[s[i]];
     }
-    node ->isWord= true;
+    node->isWord = true;
 }
 
-class Solution {
-public:
-    void getAllChilds(Trie* &root, vector<string>&words, string word) {
-    if(words.size()==3 || !root)
-       return;
-    Trie* node = root;
-    if(node && node->isWord) {
-        words.push_back(word);
-    }
-    
-    for(auto itr=node->children.begin();itr!=node->children.end();itr++) {
-        getAllChilds(itr->second, words, (word+itr->first));
-    }
-}
+class Solution
+{
+    public:
+        void getAllChilds(Trie* &root, vector<string> &words, string word)
+        {
+            if (words.size() == 3 || !root)
+                return;
+            Trie *node = root;
+            if (node && node->isWord)
+            {
+                words.push_back(word);
+            }
 
-vector<string> searchPrefix(string s, Trie * &dict) {
-    Trie* node= dict;
-    vector<string>words;
-    for(int i=0;i<s.length();i++){
-        if(!node->children[s[i]])
-            return words;
-        node = node->children[s[i]];
+            for (auto itr = node->children.begin(); itr != node->children.end(); itr++)
+            {
+                getAllChilds(itr->second, words, (word + itr->first));
+            }
+        }
+
+    vector<string> searchPrefix(string s, Trie* &dict)
+    {
+        Trie *node = dict;
+        vector<string> words;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (!node->children[s[i]])
+                return words;
+            node = node->children[s[i]];
+        }
+        if (node->isWord)
+            words.push_back(s);
+        for (auto itr = node->children.begin(); itr != node->children.end(); itr++)
+        {
+            getAllChilds(itr->second, words, s + itr->first);
+        }
+        return words;
     }
-    if(node->isWord)
-        words.push_back(s);
-    for(auto itr=node->children.begin();itr!=node->children.end();itr++) {
-        getAllChilds(itr->second, words, s+itr->first);
-    }
-    return words;
-}
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        vector<vector<string>>result;
-        if(products.size()==0)
+
+    vector<vector < string>> suggestedProducts(vector<string> &products, string searchWord)
+    {
+        vector<vector < string>> result;
+        if (products.size() == 0)
             return result;
         Trie *dict = new Trie();
-        for(int i=0;i<products.size();i++){
-            if(products[i].length()>0)
-                insert(products[i],dict);
+        for (int i = 0; i < products.size(); i++)
+        {
+            if (products[i].length() > 0)
+                insert(products[i], dict);
         }
-    
-        for(int i=0;i<searchWord.length();i++) {
-            string subString = searchWord.substr(0,i+1);
-            vector<string>words = searchPrefix(subString, dict);
+
+        for (int i = 0; i < searchWord.length(); i++)
+        {
+            string subString = searchWord.substr(0, i + 1);
+            vector<string> words = searchPrefix(subString, dict);
             result.push_back(words);
         }
         return result;
