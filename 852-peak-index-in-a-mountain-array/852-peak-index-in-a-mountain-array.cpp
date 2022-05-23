e63bc05f3afb01f683a522ed89e296366b6726b0
Hshardik1210 <104761960+Hshardik1210@users.noreply.github.com>
2022-05-24 00:35:32 +0530

    Time: 19 ms (21.11%), Space: 11.6 MB (49.58%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -7,7 +7,7 @@ public:
             int mid = low + (high - low)/2;
             if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
                 return mid;
-            else if (arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1])
+            else if (arr[mid]>arr[mid-1])
                 low = mid + 1;
             else
                 high = mid - 1;
