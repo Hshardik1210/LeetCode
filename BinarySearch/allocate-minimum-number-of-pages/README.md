4a935464e77e6fa294926be0d232819351ef2851
Hardik Sharma <hardik.sharma@whitehatjr.com>
2022-05-24 22:40:41 +0530

    binarySearch

---
+++ BinarySearch/allocate-minimum-number-of-pages/README.md
@@ -0,0 +1,15 @@
+<h2><a href="https://www.geeksforgeeks.org/allocate-minimum-number-pages/">Allocate minimum number of pages</a></h2><h3>Hard</h3><hr><div><p>Given number of pages in n different books and m students. The books are arranged in ascending order of number of pages. Every student is assigned to read some consecutive books. The task is to assign books in such a way that the maximum number of pages assigned to a student is minimum. </p>
+
+<p><strong>Example 1:</strong></p>
+
+<pre><strong>Input:</strong> pages = [1,2,3,4,5,6,7,8,9,10], student = 5
+<strong>Output:</strong> 15
+</pre>
+
+<p><strong>Example 2:</strong></p>
+
+<pre><strong>Input:</strong> pages = [3,2,2,4,1,4], student = 3
+<strong>Output:</strong> 6
+</pre>
+
+<a href="https://www.youtube.com/watch?v=2JSQIhPcHQg">Explanation Video</a>
---
+++ BinarySearch/allocate-minimum-number-of-pages/allocate-minimum-number-of-pages.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+
+        bool isValid(vector<int> &a, int n, int pages, int k)
+        {
+            int student = 1;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a[i];
+                if (sum > pages)
+                {
+                    student++;
+                    sum = a[i];
+                }
+
+                if (student > k)
+                    return false;
+            }
+            return true;
+        }
+
+    int shipWithinDays(vector<int> &a, int k)
+    {
+        int n = a.size();
+        int maximum = INT_MIN;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > maximum)
+                maximum = a[i];
+            sum += a[i];
+        }
+
+        int l = maximum;
+        int h = sum;
+        int result = -1;
+        while (l <= h)
+        {
+            int m = l + (h - l) / 2;
+            if (isValid(a, n, m, k))
+            {
+                result = m;
+                h = m - 1;
+            }
+            else
+                l = m + 1;
+        }
+        return result;
+    }
+};
