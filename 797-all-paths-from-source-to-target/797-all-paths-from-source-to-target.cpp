b54ed6d63d334a560d6eb3c851579bb8b0e4d3eb
Hshardik1210 <104761960+Hshardik1210@users.noreply.github.com>
2022-06-09 01:02:32 +0530

    Time: 11 ms (91.30%), Space: 11.9 MB (63.46%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,26 +1,32 @@
-class Solution {
-    vector<vector<int>>result;
-public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<int>visited(graph.size(),false);
-        vector<int>path;
-        int source = 0;
-        int destination = graph.size()-1;
-        dfs(graph, visited, source, destination, path);
-        return result;
-    }
-    
-    void dfs(vector<vector<int>>&graph, vector<int>&visited, int source, int destination, vector<int>&path) {
+class Solution
+{
+    vector<vector < int>> result;
+    public:
+        vector<vector < int>> allPathsSourceTarget(vector<vector < int>> &graph)
+        {
+            vector<int> visited(graph.size(), false);
+            vector<int> path;
+            int source = 0;
+            int destination = graph.size() - 1;
+            dfs(graph, visited, source, destination, path);
+            return result;
+        }
+
+    void dfs(vector<vector < int>> &graph, vector< int > &visited, int source, int destination, vector< int > &path)
+    {
 
         path.push_back(source);
-        if(source==destination) {
+        if (source == destination)
+        {
             result.push_back(path);
             return;
         }
-        
+
         visited[source] = true;
-        for(int i=0;i<graph[source].size();i++) {
-            if(!visited[graph[source][i]]) {
+        for (int i = 0; i < graph[source].size(); i++)
+        {
+            if (!visited[graph[source][i]])
+            {
                 dfs(graph, visited, graph[source][i], destination, path);
                 path.pop_back();
             }
