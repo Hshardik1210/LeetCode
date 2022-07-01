81a0fc538dd4e2140436df4b03f29c57bd6f22ac
Hardik Sharma <hardik.sharma@whitehatjr.com>
2022-07-01 22:54:42 +0530

    mycode

--- 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,50 +1,53 @@
-class RandomizedCollection
-{
-    public:
 
-        unordered_map<int, vector < int>> mp;
-    vector<pair<int, int>> nums;
-
-    RandomizedCollection() {}
-
-    bool insert(int val)
-    {
-
-        bool it = mp.find(val) == mp.end();
-
-        mp[val].push_back(nums.size());
-        nums.push_back({ val,
-            mp[val].size() - 1 });
-
-        return it;
+class RandomizedCollection {
+    unordered_map<int,int>m;
+      vector<vector<int>> v;
+public:
+    RandomizedCollection() {
+        
     }
-
-    bool remove(int val)
-    {
-
-        bool it = mp.find(val) != mp.end();
-
-        if (it)
-        {
-            auto last = nums.back();
-            mp[last.first][last.second] = mp[val].back();
-            nums[mp[val].back()] = last;
-            mp[val].pop_back();
-
-            if (mp[val].size() == 0)
-            {
-                mp.erase(val);
-            }
-
-            nums.pop_back();
+    
+    bool insert(int val) {
+        bool res = m.find(val) == m.end();
+        if(!res) {
+            int index = m[val];
+            v[index].push_back(val);
+        } else {
+            m[val] = v.size();
+            vector<int>temp;
+            temp.push_back(val);
+            v.push_back(temp);
         }
-
-        return it;
+        return res;
     }
-
-    int getRandom()
-    {
-
-        return nums[rand() % nums.size()].first;
+    
+    bool remove(int val) {
+        if(m.find(val)==m.end())
+            return false;
+        int index = m[val];
+        if(v[index].size()==1) {
+            m[v[v.size()-1][0]] = index;
+            swap(v[index],v[v.size()-1]);
+            v.pop_back();
+            m.erase(val);
+        } else {
+            v[index].pop_back();
+        }
+        return true;
+        
+    }
+    
+    int getRandom() {
+        int row = rand()%v.size();
+        int col = rand()%v[row].size();
+        return v[row][col];
     }
 };
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
