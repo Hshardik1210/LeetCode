b6dbb4c6b0c33e20c3abc9e54950f1e3e275ef20
Hshardik1210 <104761960+Hshardik1210@users.noreply.github.com>
2022-07-02 10:15:56 +0530

    Time: 140 ms (84.37%), Space: 56.4 MB (29.75%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.cpp
+++ 432-all-oone-data-structure/432-all-oone-data-structure.cpp
@@ -1,52 +1,59 @@
-class AllOne {
-    unordered_map<string,int>freq;
-    map<int,unordered_set<string>>indexMap;
-public:
-    AllOne() {
-        
-    }
-    
-    void inc(string key) {
-        if(freq.find(key)==freq.end()) {
+class AllOne
+{
+    unordered_map<string, int> freq;
+    map<int, unordered_set < string>> indexMap;
+    public:
+        AllOne() {}
+
+    void inc(string key)
+    {
+        if (freq.find(key) == freq.end())
+        {
             freq[key]++;
-            indexMap[freq[key]].insert(key);   
-        } else {
+            indexMap[freq[key]].insert(key);
+        }
+        else
+        {
             indexMap[freq[key]].erase(key);
-            if(indexMap[freq[key]].size()==0)
+            if (indexMap[freq[key]].size() == 0)
                 indexMap.erase(freq[key]);
             freq[key]++;
             indexMap[freq[key]].insert(key);
         }
     }
-    
-    void dec(string key) {
-        if(freq.find(key)==freq.end())
+
+    void dec(string key)
+    {
+        if (freq.find(key) == freq.end())
             return;
         indexMap[freq[key]].erase(key);
-        if(indexMap[freq[key]].size()==0)
+        if (indexMap[freq[key]].size() == 0)
             indexMap.erase(freq[key]);
         freq[key]--;
-        if(freq[key]==0) {
+        if (freq[key] == 0)
+        {
             freq.erase(key);
-        } 
+        }
         else
             indexMap[freq[key]].insert(key);
     }
-    
-    string getMaxKey() {
-        return indexMap.empty()? "": *((indexMap.rbegin()->second).begin());
+
+    string getMaxKey()
+    {
+        return indexMap.empty() ? "" : *((indexMap.rbegin()->second).begin());
     }
-    
-    string getMinKey() {
-        return indexMap.empty()? "": *((indexMap.begin()->second).begin());
+
+    string getMinKey()
+    {
+        return indexMap.empty() ? "" : *((indexMap.begin()->second).begin());
     }
 };
 
 /**
- * Your AllOne object will be instantiated and called as such:
- * AllOne* obj = new AllOne();
- * obj->inc(key);
- * obj->dec(key);
- * string param_3 = obj->getMaxKey();
- * string param_4 = obj->getMinKey();
+ *Your AllOne object will be instantiated and called as such:
+ *AllOne* obj = new AllOne();
+ *obj->inc(key);
+ *obj->dec(key);
+ *string param_3 = obj->getMaxKey();
+ *string param_4 = obj->getMinKey();
  */
