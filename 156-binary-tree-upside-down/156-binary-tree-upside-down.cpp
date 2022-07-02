17e26c9ec91fe866c556ff6ee884acec5704d49a
Hshardik1210 <104761960+Hshardik1210@users.noreply.github.com>
2022-07-02 22:11:04 +0530

    Time: 3 ms (79.50%), Space: 9.7 MB (19.13%) - LeetHub

--- 156-binary-tree-upside-down/156-binary-tree-upside-down.cpp
+++ 156-binary-tree-upside-down/156-binary-tree-upside-down.cpp
@@ -1,39 +1,45 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    TreeNode* upsideDownBinaryTree(TreeNode* root) {
-        
-        stack<TreeNode*>s;
-        while(root) {
-            s.push(root);
-            root=root->left;
-        }
-        
-        TreeNode* result=NULL;
-        while(!s.empty()) {
-            TreeNode* top = s.top();
-            s.pop();
-            if(!result) {
-                result = top;
+class Solution
+{
+    public:
+        TreeNode* upsideDownBinaryTree(TreeNode *root)
+        {
+
+            stack<TreeNode*> s;
+            while (root)
+            {
+                s.push(root);
+                root = root->left;
             }
-            
-            if(!s.empty()){
-            top->right = s.top();
-            top->left = s.top()->right;
-            s.top()->left = NULL;
-            s.top()->right = NULL;}
+
+            TreeNode *result = NULL;
+            while (!s.empty())
+            {
+                TreeNode *top = s.top();
+                s.pop();
+                if (!result)
+                {
+                    result = top;
+                }
+
+                if (!s.empty())
+                {
+                    top->right = s.top();
+                    top->left = s.top()->right;
+                    s.top()->left = NULL;
+                    s.top()->right = NULL;
+                }
+            }
+            return result;
         }
-        return result;
-        
-    }
 };
